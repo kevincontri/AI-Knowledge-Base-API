1ba83d4cf954aa458dd19af2a657a2a1a2c02365
kevincontri <kevin.lucas.contri@gmail.com>
2026-04-05 07:01:16 -0300

    changed server port to match render's env variable

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import uvicorn
+import os
 
 if __name__ == "__main__":
     uvicorn.run(
         "app.server.server:app",
         host="127.0.0.1",
-        port=8000,
+        port=int(os.environ.get("PORT", 8000)),
         reload=True
     )
